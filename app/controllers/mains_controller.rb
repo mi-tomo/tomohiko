3854a36976b4eabf6c9f95631a8e520455c199e3
mi-tomo <efficientcycle@gmail.com>
2019-10-26 00:51:14 +0900

    add-link

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,4 +1,10 @@
 class MainsController < ApplicationController
   def index
   end
+  def about
+  end
+  def contact
+  end
+  def works
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   root  'mains#index'
   # get 'mains' => 'mains#index'
-  resources :mains
+  resources :mains do
+    collection do
+      get "about"
+      get "contact"
+      get "works"
+      # get "step4"
+      # get "done"
+    end
+
+
+  end
 end
